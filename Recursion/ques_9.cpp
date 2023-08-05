f300777eb90e46853a8a94eb23ad096e0b6b01e4
Harsh <harshmehra53@gmail.com>
2023-08-05 19:40:09 +0530

    up

---
+++ Recursion/ques_9.cpp
@@ -0,0 +1,22 @@
+/* Check if String is palindrome or not using recursion */
+#include <bits/stdc++.h>
+using namespace std;
+
+bool f(int i, string &s)
+{
+    if (i >= s.size())
+        return true;
+    if (s[i] != s[s.size() - i - 1])
+        return false;
+    return f(i + 1, s);
+}
+
+int main()
+{
+    string s;
+    cout << "Enter the string to be checked: ";
+    cin >> s;
+
+    cout << f(0, s);
+    return 0;
+}
